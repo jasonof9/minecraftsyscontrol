2cf6ee36fa9b0d683f41c82f419875bc6d7eff0b
fenjay <jason.m.jones@gmail.com>
2018-10-28 22:35:39 +0000

    add overviewer

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template
+import boto3, json
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
-	return render_template('home.html')
+	loglines = []
+	filename = "/home/admin/mc_logs/latest.log"
+	file = open(filename, "r")
+	for line in file:
+   		loglines.append(line)
+	return render_template('home.html', value=loglines)
 
 @application.route('/about')
 def about():
 	return render_template('about.html')
 
+@application.route('/serverstatus')
+def serverstatus():
+	client = boto3.client('ec2', region_name='us-east-1')
+	response = client.describe_instance_status(InstanceIds=['i-0407af6e7e8d3ba3e'])
+	return str(response)
+
+@application.route('/overview')
+def overviewer():
+	return render_template('overview.html')
+
+@application.errorhandler(404)
+def handle404(e):
+	return e
+
+@application.errorhandler(500)
+def handle500(e):
+	return str(e)
+
 if __name__ == '__main__':
-	application.run(debug=True)
+	application.run()
