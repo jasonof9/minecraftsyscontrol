4dfab3a23551cec241daaf86787949ea89b357bd
fenjay <jason.m.jones@gmail.com>
2018-12-21 21:35:50 +0000

    add startserver and other stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, Response
 import boto3, json, os, time, settings
 
 application = Flask(__name__)
@@ -37,8 +37,37 @@ def about():
 @application.route('/serverstatus')
 def serverstatus():
 	client = boto3.client('ec2', region_name='us-east-1')
-#	response = client.describe_instance_status(InstanceIds=[settings.AWS_CONFIG['instance_id']])
-	return str(settings.AWS_CONFIG['mcinstance'])
+	response = client.describe_instance_status(InstanceIds=[settings.AWS_CONFIG['mcinstance']])
+	
+	strResponse = json.dumps(response)
+	return Response(strResponse, mimetype='text/json')
+
+
+@application.route('/startserver',methods=['GET','POST'])
+def startserver():
+	returnedData = ""
+#request.data
+#	if not returnedData:
+#		returnedData = request.args.get('hello','')
+#		if not returnedData:
+#			returnedData = 'no data'
+
+        client = boto3.client('ec2', region_name='us-east-1')
+        r = client.describe_instance_status(InstanceIds=[settings.AWS_CONFIG['mcinstance']])
+        if len(r['InstanceStatuses']) == 0:
+                i = 'instance down'
+        else:
+                i = r['InstanceStatuses'][0]['InstanceState']['Name']
+
+	if i == 'running':
+		returnedData = 'alreadyrunning'
+	else:
+		if not returnedData:
+			 result = client.start_instances(InstanceIds=[settings.AWS_CONFIG['mcinstance']])
+			 returnedData = json.dumps(result)
+#			returnedData = i
+	return render_template('startserver.html', value=returnedData)
+
 
 @application.route('/overview')
 def overviewer():
