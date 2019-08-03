f6dc726b822177b3ad4f39a072c02c38e47e656c
fenjay <jason.m.jones@gmail.com>
2019-08-03 01:56:21 +0000

    update the boilerplate

--- settings_template.py
+++ settings_template.py
@@ -1,4 +1,26 @@
-AWS_CONFIG = {
-	'mcinstance' : 'instance_id_here',
-	'otheritem' : 'value_here'
 }
+AWS_CONFIG = {
+        'mcinstance' : 'instance_id_here',
+        'mcinstance-water' : 'water_instance_id',
+        'maintenance' : '',
+        'otheritem' : 'value_here'
+}
+
+TOWN_SERVER_INFO = {
+
+        'servername' : 'a_friendly_name',
+        'bgimage' : 'http://server/bg.png',
+        'newspage' : 'news.html',
+        'aws_instance_id' : 'an_instance_id',
+        'server_address' : 'server_address'
+}
+
+WATER_SERVER_INFO = {
+
+	'servername' : 'a_friendly_name',
+        'bgimage' : 'http://server/bg.png',
+        'newspage' : 'news.html',
+        'aws_instance_id' : 'an_instance_id',
+        'server_address' : 'server_address'
+}
+
