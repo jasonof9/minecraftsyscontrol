21039152f218f58adba9f73a743970506a517e8e
Jack Jones <jackij43@outlook.com>
2023-02-17 21:53:00 -0600

    working on server dropdown

---
+++ servers.py
@@ -0,0 +1,16 @@
+SERVERS = {
+    'town' : {
+        'name' : 'Town',
+        'bgimage' : 'http://server/bg.png',
+        'news' : 'news.html',
+        'aws_instance_id' : 'the one with the minecraft server',
+        'server_address' : 'server_address'
+    },
+    'village' : {
+        'name' : 'Village',
+        'bgimage' : 'http://server/bg.png',
+        'news' : 'news.html',
+        'aws_instance_id' : 'the one with the minecraft server',
+        'server_address' : 'server_address'
+    }
+}
